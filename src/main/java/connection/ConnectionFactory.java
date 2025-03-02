ac06427121e978dee2bba07d3a7fd4c800344bc6
iristeiu <117008214+iristeiu@users.noreply.github.com>
2025-03-02 21:29:12 +0200

    Update ConnectionFactory.java

--- src/main/java/connection/ConnectionFactory.java
+++ src/main/java/connection/ConnectionFactory.java
@@ -20,7 +20,7 @@ public class ConnectionFactory {
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String DBURL = "jdbc:mysql://localhost:3306/schooldb";
     private static final String USER = "root";
-    private static final String PASS = "Cartebibelou4#";
+    private static final String PASS = "";
 
     private static ConnectionFactory singleInstance = new ConnectionFactory();
 
